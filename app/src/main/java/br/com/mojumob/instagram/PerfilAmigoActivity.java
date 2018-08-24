4477722a9f4e712745ec462834495d295b7d5b17
bytecore@gmail.com <mo10128>
2018-08-24 16:50:32 -0300

    Cria Activits iniciais

---
+++ app/src/main/java/br/com/mojumob/instagram/PerfilAmigoActivity.java
@@ -0,0 +1,13 @@
+package br.com.mojumob.instagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilAmigoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_amigo);
+    }
+}
